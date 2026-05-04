da216eda3e894d719eafb1ea02da84b8c1a4bb76
Christian01123 <christianandersen2007@gmail.com>
2026-05-04 17:31:10 +0000

    create champ

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,5 +2,6 @@ public class Launcher {
     public static void main(String[] args) throws Exception {
         // Delegate to the real ChampionsArena
         ChampionsArena.main(args);
+        MyChampion hornet = new MyChampion();
     }
 }
--- src/MyChampion.java
+++ src/MyChampion.java
@@ -5,17 +5,15 @@ import java.util.ArrayList;
  * Your champion. Change the class name, the name field, and make it yours!
  */
 public class MyChampion extends Champion {
-    public static final String NAME = "My Champion";
+    public static final String NAME = "Hornet";
 
     public MyChampion() {
-        // Create a champion with an attack of 5, a defense of 5, and max health of 40
-        super("My Champion", 5, 5, 40);
+        // Create a champion with an attack of 8, a defense of 2, and max health of 70
+        super("Hornet", 8, 2, 70);
     }
 
     @Override
     public List<Action> getActions() {
-        List<Action> actions = new ArrayList<>(super.getActions());
-        actions.add(new Jab()); // Replace with your custom actions
-        return actions;
+        return super.getActions();
     }
 }
